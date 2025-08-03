e077eb9e81f957e1537e688b259ab0f52c02110b
Prashant Kumar <prashantkumarbgs22@gmail.com>
2025-08-03 21:41:09 +0530

    Update db.php

--- include/db.php
+++ include/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost'; // Database host
-$user = 'root'; // Database username
+$host = ''; // Database host
+$user = ''; // Database username
 $pass = ''; // Database password
-$dbname = 'khana_khazana'; // Database name
+$dbname = ''; // Database name
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
